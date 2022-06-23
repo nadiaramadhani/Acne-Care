cd272942d41d1074cba35cdc2a09309844de9d95
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-06-23 11:25:13 +0700

    Latest Update Normal

---
+++ ACNIFY/TestBranch.swift
@@ -0,0 +1,20 @@
+//
+//  TestBranch.swift
+//  ACNIFY
+//
+//  Created by Rahmat Maftuh Ihsan on 23/06/22.
+//
+
+import SwiftUI
+
+struct TestBranch: View {
+    var body: some View {
+        Text("Hello Gilbert")
+    }
+}
+
+struct TestBranch_Previews: PreviewProvider {
+    static var previews: some View {
+        TestBranch()
+    }
+}
